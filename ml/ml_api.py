84e187790783451126baab16bbc59da134b74a48
lightsource <m2302070@edu.misis.ru>
2024-09-18 19:21:25 +0300

    update api

--- ml/ml_api.py
+++ ml/ml_api.py
@@ -2,10 +2,14 @@ from fastapi import FastAPI, HTTPException
 from typing import List
 from pydantic import BaseModel
 from recsys import RecSys
+from fuzzy import FuzzySearch
+
 
 app = FastAPI()
 
 recsys_instance = None
+fuzzy_search_instance = None
+
 
 class UserModel(BaseModel):
     user_id: str
@@ -13,14 +17,21 @@ class UserModel(BaseModel):
     search_history: List[str]
     interests: List[str]
 
+
 class EventModel(BaseModel):
     event_id: str
     name: str
     category: str
 
+
 class RecSysData(BaseModel):
     users_data: List[UserModel]
     events_data: List[EventModel]
+    
+    
+class FuzzySearchQuery(BaseModel):
+    query: str
+    limit: int
 
 
 @app.post('/create_recsys')
@@ -36,6 +47,17 @@ def create_recsys(recsys_data: RecSysData):
     return {'message': 'RecSys instance created successfully.'}
 
 
+@app.post('/create_fuzzysearch')
+def create_fuzzysearch(events: List[EventModel]):
+    global fuzzy_search_instance
+    if fuzzy_search_instance:
+        return {'message': 'FuzzySearch instance already exists.'}
+    
+    events_data = [event.model_dump() for event in events]
+    fuzzy_search_instance = FuzzySearch(events=events_data)
+    return {'message': 'FuzzySearch instance created successfully.'}
+
+
 @app.post('/add_user')
 def add_user(new_user: UserModel):
     global recsys_instance
@@ -45,6 +67,7 @@ def add_user(new_user: UserModel):
     recsys_instance.add_user(new_user.model_dump())
     return {'message': 'User added successfully.'}
 
+
 @app.post('/add_event')
 def add_event(new_event: EventModel):
     global recsys_instance
@@ -53,6 +76,7 @@ def add_event(new_event: EventModel):
     
     recsys_instance.add_event(new_event.model_dump())
     return {'message': 'Event added successfully.'}
+
 
 @app.put('/update_user')
 def update_user(updated_user: UserModel):
@@ -66,6 +90,7 @@ def update_user(updated_user: UserModel):
     except ValueError as e:
         raise HTTPException(status_code=404, detail=str(e))
 
+
 @app.get('/hybrid_recommendation/{user_id}')
 def hybrid_recommendation(user_id: str, top_n: int = 5):
     global recsys_instance
@@ -77,3 +102,23 @@ def hybrid_recommendation(user_id: str, top_n: int = 5):
         return {'recommended_events': recommended_events}
     except StopIteration:
         raise HTTPException(status_code=404, detail='User not found.')
+    
+    
+@app.post('/fuzzy_add_event')
+def fuzzy_add_event(event: EventModel):
+    global fuzzy_search_instance
+    if not fuzzy_search_instance:
+        raise HTTPException(status_code=400, detail='FuzzySearch instance is not initialized.')
+    
+    fuzzy_search_instance.add_event(event.name)
+    return {'message': f'Event "{event.name}" added to FuzzySearch.'}
+
+
+@app.post('/fuzzy_search')
+def fuzzy_search(query_data: FuzzySearchQuery):
+    global fuzzy_search_instance
+    if not fuzzy_search_instance:
+        raise HTTPException(status_code=400, detail='FuzzySearch instance is not initialized.')
+    
+    results = fuzzy_search_instance.search(query_data.query, limit=query_data.limit)
+    return {'results': results}
